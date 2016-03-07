af682b13a6fac7afacd242924b44c44ad8688ac6
Erin Anthony <erinmadisonanthony@gmail.com>
2016-03-07 01:19:50 +0000

    create mvc structure for blog component

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,10 @@
+class BlogsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'landing_pages#index'
+  resources :blogs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
