72246a1a5b007f806d668cf56c7d29bf430e0341
Erin Anthony <erinmadisonanthony@gmail.com>
2016-03-08 02:03:22 +0000

    connect blog and resource forms to database

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,4 +7,15 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
 
+  def create
+    Blog.create(blog_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :author, :content)
+  end
+
 end
--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -7,4 +7,15 @@ class ResourcesController < ApplicationController
     @resource = Resource.new
   end
 
+  def create
+    Resource.create(resource_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def resource_params
+    params.require(:resource).permit(:name, :website_url, :description)
+  end
+
 end
